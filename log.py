bcf3b15602427e3a9aabf4eb89456747c3019485
Della <humanita.1999@gmail.com>
2019-08-04 22:54:02 +0300

    different saving methods for new workbook and opened workbook

--- log.py
+++ log.py
@@ -1,28 +1,36 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+# NOTE:
+# - function name uses full form, i.e. "workbook", "worksheet", etc.
+# - variables and class attributes uses shorted form, i.e. "wb", "ws", etc.
 
 
 class ExcelFile:
     def __init__(self):
         self.title = ""
-        self.wb = None  # new workbook
+        self.new_wb = None  # new workbook
         self.ws = None  # new worksheet
         self.load_wb = None  # existing workbook
 
-    def new_wb(self):
+    def new_workbook(self):
         # create a new worksheet
-        self.wb = Workbook()
-        self.ws = self.wb.active
+        self.new_wb = Workbook()
+        self.ws = self.new_wb.active
+        print("new wb created")
 
-    def wb_title(self, title):
+    def workbook_title(self, title):
         # sets the month to log as title of workbook
         self.title = title
+        print(self.title, "is set title...")
 
-    def open_wb(self, filename):
+    def open_workbook(self, filename):
         # open an existing workbook
         self.load_wb = load_workbook(filename)
-        print("wb opened")
+        self.ws = self.load_wb.active
+        print("Opening workbook...")
 
-    def initialise_ws(self):
+    def initialise_worksheet(self):
         # setting up workbook headers
 
         date_c_header = self.ws['A1']
@@ -41,23 +49,53 @@ class ExcelFile:
         self.ws.cell(column=1, row=next_row, value=date)
         self.ws.cell(column=2, row=next_row, value=location)
         self.ws.cell(column=3, row=next_row, value=amount)
+        print("Added log...")
 
     def find_last_row_used(self):
         # searches for the last row entered
         return self.ws.max_row
 
-    def save_wb(self):
+    def save_new_workbook(self):
+        # saves workbook
         file_name = self.title + ".xlsx"
-        self.wb.save(file_name)
+        self.new_wb.save(file_name)
+
+    def save_opened_workbook(self):
+        file_name = self.title + ".xlsx"
+        self.load_wb.save(file_name)
+        print(file_name, "is the saved file.")
+
+    def view(self):  #todo
+        # iterate over used cells in Excel file
+        # add to data structure -- ?
+        # convert to string
+        # print in separate window
+
+        self.ws = self.load_wb.get_sheet_by_name("Sheet")
+        print(self.ws)
+        file_data = {}
+        print("Reading rows...")
+        for row in range(2, self.ws.max_row + 1):
+            date = self.ws['A' + str(row)].value
+            location = self.ws['B' + str(row)].value
+            amount = self.ws['C' + str(row)].value
+
+            file_data.setdefault(date, {location, amount})
+            print(file_data)
+
+# xl_new = ExcelFile()
+# xl_new.new_workbook()
+# xl_new.workbook_title("testfile")
+# xl_new.initialise_worksheet()
+# xl_new.add_log('01/02/2019','No Frills','30')
+# xl_new.add_log('02/03/2019','no','10')
+# xl_new.add_log('03/04/2019', 'Chapters', '100')
+#
+# xl_new.save_workbook()
 
 
 # xl = ExcelFile()
-# xl.new_wb()
-# xl.wb_title("testfile")
-# xl.initialise_ws()
-# xl.add_log('01/02/2019','No Frills','30')
-# xl.add_log('02/03/2019','no','10')
-# xl.add_log('03/04/2019', 'Chapters', '100')
-# #xl.add_log_v2()
-# xl.save_wb()
-
+# xl.open_workbook('May.xlsx')
+# xl.workbook_title('May')
+# xl.add_log("02/02/2019", "Shoppers", "15")
+# xl.save_opened_workbook()
